0eb43a827627ad287aae9b08fda490e6d6c37da0
test-q <sr.test85@gmail.com>
2020-11-05 14:31:28 +0530

    Registrion page added

---
+++ src/main/java/com/qa/crm/pages/RegiPage.java
@@ -0,0 +1,15 @@
+package com.qa.crm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.crm.utils.ElementUtils;
+
+public class RegiPage {
+
+	private WebDriver driver;
+	
+	//Page constructor
+	public RegiPage(WebDriver driver) {
+		this.driver = driver;
+	}
+}
